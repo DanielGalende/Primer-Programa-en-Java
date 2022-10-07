ee19e97be3bf590caa793fc47dc6ee5de210f8b2
Daniel <danigalcol@gmail.com>
2022-10-07 11:14:44 +0200

    Mi año de nacimiento y edad

--- Prueba.java
+++ Prueba.java
@@ -2,6 +2,8 @@ public class Prueba {
 	public static void main (String args[]) {
 		System.out.println("Galende");
 		System.out.println("Colino");
+		System.out.println("18 años");
+		System.out.println("2004");
 
 
 
