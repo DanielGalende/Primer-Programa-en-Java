87c7b5c91686c293c35479d3f3167b019c43bcc2
Daniel <danigalcol@gmail.com>
2022-10-10 09:31:25 +0200

    Mi grupo gfavorito

--- Prueba.java
+++ Prueba.java
@@ -1,11 +1,10 @@
 public class Prueba {
 	public static void main (String args[]) {
-		System.out.println("Galende");
-		System.out.println("Colino");
+		
 		System.out.println("18 años");
 		System.out.println("2004");
 		System.out.println("Pizza");
-
+		System.out.println("NEFFEX");
 
 
 
